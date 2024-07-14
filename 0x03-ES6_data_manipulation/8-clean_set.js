e0c1932a821b6d729d1f8803b53c4e7b88f6f30d
Thabang Maphangisane <thabangh647@gmail.com>
2024-07-14 17:22:25 +0200

    8. Clean set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,15 +1,12 @@
 export default function cleanSet(set, startString) {
   if (startString && typeof startString === 'string') {
     const strings = [];
-
     set.forEach((item) => {
       if (item.toLowerCase().startsWith(startString.toLowerCase())) {
         strings.push(item.slice(startString.length));
       }
     });
-
     return strings.join('-');
   }
-
   return '';
 }
