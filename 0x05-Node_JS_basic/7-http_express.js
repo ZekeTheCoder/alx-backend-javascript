40aa435f90f26802d1171923cbdccd9feb4464ff
Thabang Maphangisane <thabangh647@gmail.com>
2024-08-21 04:05:14 +0200

    7. Create a more complex HTTP server using Express3

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,28 +1,26 @@
-#!/usr/bin/env node
 const express = require('express');
 const countStudents = require('./3-read_file_async');
 
 const app = express();
-const hostname = '127.0.0.1';
 const port = 1245;
 const databasePath = process.argv[2];
 
 app.get('/', (request, response) => {
-  response.send('Hello Holberton School!');
+  response.status(200).send('Hello Holberton School!');
 });
 
 app.get('/students', (request, response) => {
   countStudents(databasePath)
     .then((output) => {
-      response.send(['This is the list of our students', output].join('\n'));
+      response.status(200).send(['This is the list of our students', output].join('\n'));
     })
     .catch((error) => {
       response.status(500).send(error.message);
     });
 });
 
-app.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.listen(port, () => {
+  console.log(`Server running at http://127.0.0.1:${port}/`);
 });
 
 module.exports = app;
